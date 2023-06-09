d5fe11a52473603b00b5f4d285e539e8e5b85803
antonmyronyuk <antonmyronyuk@gmail.com>
2023-06-09 16:23:09 +0300

    Upd test

--- tests/test_estimator.py
+++ tests/test_estimator.py
@@ -8,10 +8,7 @@ from quantile_estimator import Estimator
 @pytest.mark.parametrize("num_observations", [1, 10, 100, 1000, 10000, 100000])
 def test_random_observations(num_observations):
     estimator = Estimator()
-    sum_observations = 0
     for _ in range(num_observations):
-        value = random.randint(1, 1000)
-        estimator.observe(value)
-        sum_observations += value
+        estimator.observe(random.randint(1, 1000) / 100)
 
     assert 1 <= estimator.query(0.5) <= estimator.query(0.9) <= estimator.query(0.99) <= 1000
